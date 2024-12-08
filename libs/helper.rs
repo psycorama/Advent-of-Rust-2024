b1545a1c5e2172ce15cd1456f8e80f2c11e9e778
Andreas Geisenhainer <psycorama@datenhalde.de>
2024-12-08 21:10:33 +0100

    feat: add new helper to get 2d-vector (of char) from file

--- libs/helper.rs
+++ libs/helper.rs
@@ -11,3 +11,15 @@ pub fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .map(|l| l.expect("unable to parse line"))
         .collect()
 }
+
+
+pub fn get_2d_vector_from_file(filename: impl AsRef<Path>) ->Vec<Vec<char>> {
+
+    let buffer = lines_from_file(filename);
+
+    let mut input: Vec<Vec<char>> = vec![];
+    for line in buffer {
+        input.push( line.chars().collect() );
+    }
+    input
+}
